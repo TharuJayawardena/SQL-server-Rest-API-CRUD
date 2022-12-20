45f40906daa83959ff6a15f313abe49a265dc88d
TharushiJayawardena <tharushijayawardena03@gmail.com>
2022-12-20 23:35:14 +0530

    first commit

---
+++ data/events/eventslist.sql
@@ -0,0 +1,2 @@
+SELECT [eventId],[eventTitle],[eventDescription],[startDate],[endDate],[avebue],[maxMembers]
+FROM [dbo].[events]
---
+++ routes/eventRoutes.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const express = require('express');
+const eventController = require('../controllers/eventController');
+
+const router = express.Router();
+
+
+const {getEvents,getEvent,addEvent,updateEvent,deleteEvent} = eventController;
+
+router.get('/events',getEvents);
+router.get('/event/:id',getEvent);
+router.post('/event',addEvent);
+router.put('/event/:id',updateEvent);
+router.get('/event/:id',deleteEvent);
+
+module.exports = {
+    routes: router
+}
